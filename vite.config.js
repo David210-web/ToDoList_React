007490b8d227325ed4843bf9b7a7ff1caeddf61c
David Castillo <cdavidrigoberto@gmail.com>
2024-07-23 20:20:22 -0600

    configurando github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base: 'https://David210-web.github.io/ToDoList-React.git'
+  base: 'https://github.com/David210-web/ToDoList-React/'
 })
